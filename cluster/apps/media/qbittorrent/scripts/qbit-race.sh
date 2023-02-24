ea09d7a9761888a9d4c580882003bbbdacd20c64
Steven Kreitzer <skre@skre.me>
2023-02-24 17:01:22 -0600

    updating manifests

---
+++ cluster/apps/media/qbittorrent/scripts/qbit-race.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sed -i 's/http\:\/\/dl-cdn.alpinelinux.org/http\:\/\/mirror.clarkson.edu/g' /etc/apk/repositories
+
+echo "**** installing nodejs + git ****"
+apk add --update nodejs npm git
+
+echo "**** installing qbit-race ****"
+rm -rf /config/qbit-race
+git clone https://github.com/buroa/qbit-race.git /config/qbit-race && \
+  cd /config/qbit-race && \
+  npm install . && \
+  npm link
