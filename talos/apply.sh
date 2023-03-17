23f7b97dcfed0b65936ca2867d874e4e7bfd6339
Steven Kreitzer <skre@skre.me>
2023-03-17 15:52:54 -0500

    updating manifests

---
+++ talos/apply.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+talhelper genconfig
+
+for i in {0..2}
+do
+   NAME="m$i"
+   ENDPOINT="10.0.0.1$i"
+   echo -n "$NAME - $ENDPOINT: "
+   talosctl -n $ENDPOINT apply-config -f clusterconfig/k8s-$NAME*.yaml
+done
+
+for i in {0..1}
+do
+   NAME="w$i"
+   ENDPOINT="10.0.0.2$i"
+   echo -n "$NAME - $ENDPOINT: "
+   talosctl -n $ENDPOINT apply-config -f clusterconfig/k8s-$NAME*.yaml
+done
