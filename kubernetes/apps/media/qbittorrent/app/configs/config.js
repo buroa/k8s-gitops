fe04a2f18eee3a0445fefab45c544d11c1890297
buroa <skre@skre.me>
2023-06-10 09:47:01 -0500

    feat: trying out risky xseed matching

--- kubernetes/apps/media/qbittorrent/app/configs/config.js
+++ kubernetes/apps/media/qbittorrent/app/configs/config.js
@@ -1,5 +1,7 @@
 module.exports = {
   delay: 30,
+  rssCadence: "15 minutes", // autobrr doesnt get every announcement
+  qbittorrentUrl: "http://localhost:8080",
 
   torznab: [
     "http://prowlarr.media.svc.cluster.local:9696/3/api?apikey={{ .PROWLARR_APIKEY }}", // ipt
@@ -15,13 +17,16 @@ module.exports = {
   ],
 
   action: "inject",
-  matchMode: "safe",
-  skipRecheck: true,
   includeEpisodes: true,
   includeNonVideos: true,
   duplicateCategories: true,
+
+  matchMode: "risky",
+  skipRecheck: false,
+  linkType: "symlink",
+  linkDir: "/media/downloads/torrent/xseeds",
+  dataDirs: ["/media/downloads/torrent/complete"],
+
   outputDir: "/config/xseeds",
   torrentDir: "/config/qBittorrent/BT_backup",
-  qbittorrentUrl: "http://localhost:8080",
-  rssCadence: "15 minutes", // autobrr doesnt get every announcement
 };
