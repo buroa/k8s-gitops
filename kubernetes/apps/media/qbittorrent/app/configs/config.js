6a689c292032d706fa212424d86fc556a163f09a
buroa <skre@skre.me>
2023-06-15 09:15:35 -0500

    fix: try risky

--- kubernetes/apps/media/qbittorrent/app/configs/config.js
+++ kubernetes/apps/media/qbittorrent/app/configs/config.js
@@ -1,3 +1,8 @@
+// Torrent content layout: Original
+// Default Torrent Management Mode: Automatic
+// Default Save Path: /media/downloads/torrents
+// External program on finished: /scripts/xseed.sh "%F"
+
 module.exports = {
   delay: 30,
   qbittorrentUrl: "http://localhost:8080",
@@ -18,13 +23,16 @@ module.exports = {
 
   action: "inject",
   includeEpisodes: true,
-  includeNonVideos: true,
+  includeNonVideos: false,
   duplicateCategories: true,
 
-  matchMode: "risky",
+  matchMode: "safe",
   skipRecheck: true,
   linkType: "symlink",
   linkDir: "/media/downloads/torrents/xseeds",
+
+  // I have sonarr, radarr, and prowlarr categories set in qBittorrent
+  // The save paths fpr them are set to the following:
   dataDirs: [
     "/media/downloads/torrents/sonarr",
     "/media/downloads/torrents/radarr",
