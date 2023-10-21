9c63a23e0752065832a0f2918ae9f1d2563c9b7e
Steven Kreitzer <skre@skre.me>
2023-10-21 07:05:37 -0500

    feat: add back xseed on completed

--- kubernetes/apps/media/qbittorrent/app/configs/config.js
+++ kubernetes/apps/media/qbittorrent/app/configs/config.js
@@ -2,9 +2,10 @@
 // Default Torrent Management Mode: Automatic
 // Default Save Path: /media/downloads/torrents/complete
 // Incomplete Save Path: /media/downloads/torrents/incomplete
+// External program on finished: /scripts/xseed.sh "%F"
 
 module.exports = {
-  delay: 15,
+  delay: 20,
   qbittorrentUrl: "http://localhost:8080",
 
   torznab: [
@@ -28,10 +29,22 @@ module.exports = {
   action: "inject",
   matchMode: "safe",
   skipRecheck: true,
-
-  includeEpisodes: true,
-  includeNonVideos: true,
   duplicateCategories: true,
+
+  includeNonVideos: true,
+  includeEpisodes: false,
+  includeSingleEpisodes: true,
+
+  // I have sonarr, radarr, and manual categories set in qBittorrent
+  // The save paths for them are set to the following:
+  dataDirs: [
+    "/media/downloads/torrents/complete/sonarr",
+    "/media/downloads/torrents/complete/radarr",
+    "/media/downloads/torrents/complete/manual",
+  ],
+
+  linkType: "hardlink",
+  linkDir: "/media/downloads/torrents/complete/xseed",
 
   outputDir: "/config/xseed",
   torrentDir: "/config/qBittorrent/BT_backup",
