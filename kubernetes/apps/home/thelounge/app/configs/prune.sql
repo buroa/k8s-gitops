b06465f8bea489444c696ca3114b9a78aa6763cb
Steven Kreitzer <skre@skre.me>
2023-11-22 14:12:39 -0600

    chore(thelounge): remove glue

--- kubernetes/apps/home/thelounge/app/configs/prune.sql
+++
@@ -1,7 +0,0 @@
-pragma journal_mode=memory;
-
--- Description: Prune old messages from the database
--- https://github.com/thelounge/thelounge/wiki/Purging-logs-older-than-X-days
-delete from messages where time < strftime('%s', datetime('now', '-7 day'))*1000;
-
-VACUUM;
