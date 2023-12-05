7ba20090b947f9a42000de4f3cdf735e97d9c25f
Steven Kreitzer <skre@skre.me>
2023-12-05 08:57:22 -0600

    chore(qbittorrent): xseed use prowlarr api key as env

--- kubernetes/apps/media/qbittorrent/app/configs/config.js
+++ kubernetes/apps/media/qbittorrent/app/configs/config.js
@@ -5,25 +5,26 @@
 
 module.exports = {
   delay: 20,
+  port: process.env.PORT || 2468,
   qbittorrentUrl: "http://localhost:8080",
 
   torznab: [
-    "http://prowlarr.media.svc.cluster.local:9696/114/api?apikey={{ .PROWLARR_API_KEY }}", // atr
-    "http://prowlarr.media.svc.cluster.local:9696/80/api?apikey={{ .PROWLARR_API_KEY }}",  // ar
-    "http://prowlarr.media.svc.cluster.local:9696/77/api?apikey={{ .PROWLARR_API_KEY }}",  // ant
-    "http://prowlarr.media.svc.cluster.local:9696/16/api?apikey={{ .PROWLARR_API_KEY }}",  // blu
-    "http://prowlarr.media.svc.cluster.local:9696/11/api?apikey={{ .PROWLARR_API_KEY }}",  // fl
-    "http://prowlarr.media.svc.cluster.local:9696/43/api?apikey={{ .PROWLARR_API_KEY }}",  // hds
-    "http://prowlarr.media.svc.cluster.local:9696/5/api?apikey={{ .PROWLARR_API_KEY }}",   // hdt
-    "http://prowlarr.media.svc.cluster.local:9696/3/api?apikey={{ .PROWLARR_API_KEY }}",   // ipt
-    "http://prowlarr.media.svc.cluster.local:9696/7/api?apikey={{ .PROWLARR_API_KEY }}",   // mtv
-    "http://prowlarr.media.svc.cluster.local:9696/17/api?apikey={{ .PROWLARR_API_KEY }}",  // ptp
-    "http://prowlarr.media.svc.cluster.local:9696/44/api?apikey={{ .PROWLARR_API_KEY }}",  // phd
-    "http://prowlarr.media.svc.cluster.local:9696/15/api?apikey={{ .PROWLARR_API_KEY }}",  // st
-    "http://prowlarr.media.svc.cluster.local:9696/78/api?apikey={{ .PROWLARR_API_KEY }}",  // td
-    "http://prowlarr.media.svc.cluster.local:9696/6/api?apikey={{ .PROWLARR_API_KEY }}",   // ts
-    "http://prowlarr.media.svc.cluster.local:9696/4/api?apikey={{ .PROWLARR_API_KEY }}",   // tl
-    "http://prowlarr.media.svc.cluster.local:9696/8/api?apikey={{ .PROWLARR_API_KEY }}",   // uhdb
+    `http://prowlarr.media.svc.cluster.local:9696/114/api?apikey=${process.env.PROWLARR_API_KEY}`, // atr
+    `http://prowlarr.media.svc.cluster.local:9696/80/api?apikey=${process.env.PROWLARR_API_KEY}`,  // ar
+    `http://prowlarr.media.svc.cluster.local:9696/77/api?apikey=${process.env.PROWLARR_API_KEY}`,  // ant
+    `http://prowlarr.media.svc.cluster.local:9696/16/api?apikey=${process.env.PROWLARR_API_KEY}`,  // blu
+    `http://prowlarr.media.svc.cluster.local:9696/11/api?apikey=${process.env.PROWLARR_API_KEY}`,  // fl
+    `http://prowlarr.media.svc.cluster.local:9696/43/api?apikey=${process.env.PROWLARR_API_KEY}`,  // hds
+    `http://prowlarr.media.svc.cluster.local:9696/5/api?apikey=${process.env.PROWLARR_API_KEY}`,   // hdt
+    `http://prowlarr.media.svc.cluster.local:9696/3/api?apikey=${process.env.PROWLARR_API_KEY}`,   // ipt
+    `http://prowlarr.media.svc.cluster.local:9696/7/api?apikey=${process.env.PROWLARR_API_KEY}`,   // mtv
+    `http://prowlarr.media.svc.cluster.local:9696/17/api?apikey=${process.env.PROWLARR_API_KEY}`,  // ptp
+    `http://prowlarr.media.svc.cluster.local:9696/44/api?apikey=${process.env.PROWLARR_API_KEY}`,  // phd
+    `http://prowlarr.media.svc.cluster.local:9696/15/api?apikey=${process.env.PROWLARR_API_KEY}`,  // st
+    `http://prowlarr.media.svc.cluster.local:9696/78/api?apikey=${process.env.PROWLARR_API_KEY}`,  // td
+    `http://prowlarr.media.svc.cluster.local:9696/6/api?apikey=${process.env.PROWLARR_API_KEY}`,   // ts
+    `http://prowlarr.media.svc.cluster.local:9696/4/api?apikey=${process.env.PROWLARR_API_KEY}`,   // tl
+    `http://prowlarr.media.svc.cluster.local:9696/8/api?apikey=${process.env.PROWLARR_API_KEY}`,   // uhdb
   ],
 
   apiAuth: false,
