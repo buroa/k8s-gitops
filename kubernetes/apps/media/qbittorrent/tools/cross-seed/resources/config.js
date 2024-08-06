83e7900aded37d6f0fa1a9ba5cb5e2a53fc0be72
Steven Kreitzer <skre@skre.me>
2024-08-06 08:07:48 -0500

    feat(cross-seed): deploy again

---
+++ kubernetes/apps/media/qbittorrent/tools/cross-seed/resources/config.js
@@ -0,0 +1,24 @@
+// Torrent content layout: Original
+// Default Torrent Management Mode: Automatic
+// Default Save Path: /media/downloads/torrents/complete
+// Incomplete Save Path: /media/downloads/torrents/incomplete
+
+module.exports = {
+  action: "inject",
+  apiKey: process.env.CROSS_SEED_API_KEY,
+  delay: 30,
+  duplicateCategories: true,
+  flatLinking: true,
+  includeEpisodes: true,
+  includeNonVideos: true,
+  includeSingleEpisodes: true,
+  linkCategory: "cross-seed",
+  linkDir: "/media/downloads/torrents/complete/cross-seed",
+  linkType: "hardlink",
+  matchMode: "safe",
+  outputDir: "/config",
+  qbittorrentUrl: "http://qbittorrent.media.svc.cluster.local:8080",
+  skipRecheck: true,
+  torrentDir: "/config/qBittorrent/BT_backup",
+  torznab: [], // Only using annoucements from autobrr
+};
