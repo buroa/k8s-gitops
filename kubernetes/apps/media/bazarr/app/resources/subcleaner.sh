9a83e603bde619d391860dd1482e44905b3c41a0
Steven Kreitzer <skre@skre.me>
2025-01-10 09:25:36 -0600

    chore(flux): remove timezone var

--- kubernetes/apps/media/bazarr/app/resources/subcleaner.sh
+++ kubernetes/apps/media/bazarr/app/resources/subcleaner.sh
@@ -12,7 +12,7 @@ if [[ -n "$section" ]]; then
     printf "Refreshing Plex section '%s' for '%s' ...\n" "$section" "$(dirname "$1")"
     /usr/bin/curl -I -X GET -G \
         --data-urlencode "path=$(dirname "$1")" \
-        --data-urlencode "X-Plex-Token=$${PLEX_TOKEN}" \
+        --data-urlencode "X-Plex-Token=${PLEX_TOKEN}" \
         --no-progress-meter \
-            "http://plex.media.svc.cluster.local:32400/library/sections/$${section}/refresh"
+            "http://plex.media.svc.cluster.local:32400/library/sections/${section}/refresh"
 fi
