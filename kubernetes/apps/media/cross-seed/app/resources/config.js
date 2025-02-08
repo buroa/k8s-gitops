f50d468c7944e62a9ab15e6952e733d618d6b87b
Steven Kreitzer <skre@skre.me>
2025-02-08 09:21:15 -0600

    chore(cross-seed): better js env

--- kubernetes/apps/media/cross-seed/app/resources/config.js
+++ kubernetes/apps/media/cross-seed/app/resources/config.js
@@ -19,14 +19,14 @@ module.exports = {
   port: Number(process.env.CROSS_SEED_PORT),
   qbittorrentUrl: "http://qbittorrent.media.svc.cluster.local:8080",
   radarr: [
-    "http://radarr.media.svc.cluster.local:7878/?apikey=" + process.env.RADARR_API_KEY,
+    `http://radarr.media.svc.cluster.local:7878/?apikey=${process.env.RADARR_API_KEY}`,
   ],
   skipRecheck: true,
   sonarr: [
-    "http://sonarr.media.svc.cluster.local:8989/?apikey=" + process.env.SONARR_API_KEY,
+    `http://sonarr.media.svc.cluster.local:8989/?apikey=${process.env.SONARR_API_KEY}`,
   ],
   torznab: [
-    "http://prowlarr.media.svc.cluster.local:9696/320/api?apikey=" + process.env.PROWLARR_API_KEY,
+    `http://prowlarr.media.svc.cluster.local:9696/320/api?apikey=${process.env.PROWLARR_API_KEY}`,
   ],
   useClientTorrents: true,
 };
