80f90d22431f9b41ec7564fc0a7f4ebb7dae451d
Steven Kreitzer <skre@skre.me>
2025-02-10 10:15:34 -0600

    feat(bootstrap): all into one script

---
+++ kubernetes/bootstrap/hooks/cilium-config.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+function log() {
+    echo -e "\033[0;32m[$(date --iso-8601=seconds)] (${FUNCNAME[1]}) $*\033[0m"
+}
+
+function wait_for_crds() {
+    local -r crds=(
+        "ciliuml2announcementpolicies.cilium.io"
+        "ciliumbgppeeringpolicies.cilium.io"
+        "ciliumloadbalancerippools.cilium.io"
+    )
+
+    for crd in "${crds[@]}"; do
+        until kubectl get crd "${crd}" &>/dev/null; do
+            log "Waiting for Cilium CRD '${crd}'..."
+            sleep 5
+        done
+    done
+}
+
+function apply_config() {
+    if kubectl diff \
+        --namespace=kube-system \
+        --kustomize \
+        "../apps/kube-system/cilium/config" &>/dev/null;
+    then
+        log "Cilium config is up to date. Skipping..."
+    else
+        log "Applying Cilium config..."
+        kubectl apply \
+            --namespace=kube-system \
+            --server-side \
+            --field-manager=kustomize-controller \
+            --kustomize \
+            "../apps/kube-system/cilium/config"
+    fi
+}
+
+function main() {
+    wait_for_crds
+    apply_config
+}
+
+main "$@"
