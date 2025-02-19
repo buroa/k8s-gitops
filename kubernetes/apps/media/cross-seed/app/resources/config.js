4207de6aaf15d7eb813ce1eb72418e0cfe905f54
Steven Kreitzer <skre@skre.me>
2025-02-18 18:28:59 -0600

    chore(cross-seed): i have enough xseeds

--- kubernetes/apps/media/cross-seed/app/resources/config.js
+++ kubernetes/apps/media/cross-seed/app/resources/config.js
@@ -10,23 +10,13 @@ module.exports = {
   includeNonVideos: true,
   includeSingleEpisodes: true,
   linkCategory: "cross-seed",
-  linkDirs: [
-    "/media/downloads/torrents/complete/cross-seed"
-  ],
+  linkDirs: ["/media/downloads/torrents/complete/cross-seed"],
   linkType: "hardlink",
   matchMode: "partial",
   outputDir: "/tmp",
   port: Number(process.env.CROSS_SEED_PORT),
   qbittorrentUrl: "http://qbittorrent.media.svc.cluster.local:8080",
-  radarr: [
-    `http://radarr.media.svc.cluster.local:7878/?apikey=${process.env.RADARR_API_KEY}`,
-  ],
   skipRecheck: true,
-  sonarr: [
-    `http://sonarr.media.svc.cluster.local:8989/?apikey=${process.env.SONARR_API_KEY}`,
-  ],
-  torznab: [
-    `http://prowlarr.media.svc.cluster.local:9696/320/api?apikey=${process.env.PROWLARR_API_KEY}`,
-  ],
+  torznab: [], // Only using announcements from autobrr
   useClientTorrents: true,
 };
